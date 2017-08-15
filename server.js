d575a00e1993fc54d8eb187001520e71193a24fb
pmd111222 <30722327+pmd111222@users.noreply.github.com>
2017-08-15 15:28:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
+//app.get('/ui/madi.png', function (req, res) {
   //res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/', function (req, res) {
+  
 res.sendFile('no image here!');
     
 });
